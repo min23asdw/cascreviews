07c8fcde3a3bbb369aa8e1d6c48dde6ad4be49d7
min23asdw <76154445+min23asdw@users.noreply.github.com>
2024-08-05 09:24:29 +0700

    init: tailwindcss and mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+// import { ThemeProvider } from '@mui/material'
+import { ThemeProvider } from '@mui/system';
+import theme from "./theme";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+                <AppRouterCacheProvider>
+                <ThemeProvider theme={theme}>
+                  {children}
+                  </ThemeProvider>
+                </AppRouterCacheProvider>
+                </body>
     </html>
   );
 }
